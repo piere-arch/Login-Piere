a91af7a264d9e68844044a8655540a496d7c1c72
Pierediaz Putra <pierediaz.putra@gmail.com>
2023-07-05 12:04:52 +0700

    first commit

---
+++ login-pier/resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+    <html>
+        <body>
+            <p> Iam Me </p>
+            <p style="color:red;">Iam Red</p>
+            <p style="color:blue;">Iam Blue</p>
+            <p style="font-size:50px;">Iam Groot</p>
+
+</body>
+</html>
+
+
