eaaf5aad71aa90eee688afcfedfea571816950bd
Pierediaz Putra <pierediaz.putra@gmail.com>
2023-07-06 12:48:58 +0700

    Laravel

--- login-pier/routes/web.php
+++ login-pier/routes/web.php
@@ -14,7 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+
+Route::get('/awal', function () {
+    return view('index');
+});
+
+Route::get('/login-test', function () {
+    return view('login');
 });
 
 Auth::routes();
